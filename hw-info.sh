1b05a86de721fec1a736c8d33960fe7853b26038
Echo-Head-Wall <101990236+Echo-Head-Wall@users.noreply.github.com>
2024-08-29 02:38:42 -0400

    Update hw-info.sh
    removed some extra calls for this one i only really care about the HW

--- hw-info.sh
+++ hw-info.sh
@@ -19,31 +19,15 @@ echo "=================="
 
 # Hostname
 echo "Hostname: $(hostname)"
-
-# OS Information
-echo -e "\nOS Information:"
-run_command "cat" "cat /etc/os-release | grep -E '^(NAME|VERSION)='"
-
-# Kernel Version
-echo -e "\nKernel Version:"
-uname -r
-
 # CPU Information
 echo -e "\nCPU Information:"
 run_command "lscpu" "lscpu | grep -E 'Model name|Socket|Core|Thread|CPU MHz'"
-
 # Memory Information
 echo -e "\nMemory Information:"
 run_command "free" "free -h"
-
 # Disk Information
 echo -e "\nDisk Information:"
 run_command "df" "df -h"
-
-# Network Information
-echo -e "\nNetwork Information:"
-run_command "ip" "ip addr | grep -E 'inet |ether'"
-
 # System Manufacturer and Model (requires root privileges)
 echo -e "\nSystem Manufacturer and Model:"
 if [ "$(id -u)" -eq 0 ]; then
@@ -51,25 +35,6 @@ if [ "$(id -u)" -eq 0 ]; then
 else
     echo "Root privileges required to display system manufacturer and model."
 fi
-
-# BIOS Information (requires root privileges)
-echo -e "\nBIOS Information:"
-if [ "$(id -u)" -eq 0 ]; then
-    run_command "dmidecode" "dmidecode -t bios | grep -E 'Vendor|Version|Release Date'"
-else
-    echo "Root privileges required to display BIOS information."
-fi
-
 # GPU Information
 echo -e "\nGPU Information:"
 run_command "lspci" "lspci | grep -i vga"
-
-# USB Devices
-echo -e "\nUSB Devices:"
-run_command "lsusb" "lsusb"
-
-# NUMA Information
-echo -e "\nNUMA Information:"
-run_command "lscpu" "lscpu | grep -i numa"
-
-echo -e "\nNote: This script displays system information. Be cautious about sharing this output as it may contain sensitive data."
